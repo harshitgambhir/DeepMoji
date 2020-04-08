8d926e4cfb900ce7eb1e5d97cf6da43ba5140e12
harshitgambhir <harshitgambhir88@gmail.com>
2020-04-08 17:46:22 +0530

    upgraded versions

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
         'emoji==0.4.5',
         'h5py==2.7.0',
         'Keras==2.0.9',
-        'numpy==1.13.1',
-        'scikit-learn==0.19.0',
+        'numpy==1.18.2',
+        'scikit-learn==1.4.1',
         'text-unidecode==1.0',
     ],
 )

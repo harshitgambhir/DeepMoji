499e4c9ab7348dd2f2f636786752932e59bf3488
harshitgambhir <harshitgambhir88@gmail.com>
2020-04-08 18:48:35 +0530

    upgraded versions

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name='deepmoji',
     version='1.0',
-    packages=['deepmoji'],
     description='DeepMoji library',
     include_package_data=True,
     install_requires=[

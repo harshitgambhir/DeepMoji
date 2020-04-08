cc30e2c0740b998f64996ced88f63c29dc41f83d
harshitgambhir <harshitgambhir88@gmail.com>
2020-04-08 20:11:38 +0530

    upgraded versions

--- deepmoji/global_variables.py
+++ deepmoji/global_variables.py
@@ -1,7 +1,7 @@
 """ Global variables.
 """
 import tempfile
-from os.path import abspath, dirname
+from os.path import abspath, dirname, realpath
 
 # The ordering of these special tokens matter
 # blank tokens can be used for new purposes
@@ -15,7 +15,7 @@ SPECIAL_TOKENS = ['CUSTOM_MASK',
                   'CUSTOM_BREAK']
 SPECIAL_TOKENS.extend(['{}BLANK_{}'.format(SPECIAL_PREFIX, i) for i in range(6, 10)])
 
-ROOT_PATH = dirname(dirname(abspath(__file__)))
+ROOT_PATH = dirname(realpath(__file__))
 VOCAB_PATH = '{}/model/vocabulary.json'.format(ROOT_PATH)
 PRETRAINED_PATH = '{}/model/deepmoji_weights.hdf5'.format(ROOT_PATH)
 
